bdffd239026931592619658460da02f171122592
Agustin Chiappe Berrini <jnieve@gmail.com>
2017-10-01 08:34:46 -0400

    Added vigenere cipher

--- haskell/Cipher/src/Lib.hs
+++ haskell/Cipher/src/Lib.hs
@@ -1,6 +1,7 @@
 module Cipher(
   caesar,
-  unCaesar) where
+  unCaesar,
+  vigenere) where
 
 import Data.Char
 
@@ -16,7 +17,6 @@ shift n c
   | isUpper c = shiftWithBase c n upperBase
   | otherwise = c
   where
-    shiftWithBase :: Char -> Integer -> Integer -> Char
     shiftWithBase c n base = integerChr $ (mod (integerOrd c - base + n) 26) + base
 
 caesar :: Integer -> String -> String
@@ -24,3 +24,18 @@ caesar n = map $ shift n
 
 unCaesar :: Integer -> String -> String
 unCaesar n = map $ shift (-n)
+
+vigenere :: String -> String -> String
+vigenere key "" = ""
+vigenere key phrase = go key phrase 0
+  where
+    go _ "" _ = ""
+    go key (c:cs) step
+      | isLower c || isUpper c = shift (getShiftFromChar key step) c : go key cs (step + 1)
+      | otherwise = c : go key cs step
+    getShiftFromChar key step = getShift $ getChar key step
+    getShift c
+      | isLower c = (integerOrd c) - lowerBase
+      | isUpper c = (integerOrd c) - upperBase
+      | otherwise = 0
+    getChar key step = key !! ((fromInteger step) `mod` (length key))
