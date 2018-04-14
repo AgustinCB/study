9790a133aac4257f0ac654e10d2e915fb72d0535
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-04-14 13:38:41 -0400

    clean up things

--- os/ostep/chap22/generate_data.sh
+++ os/ostep/chap22/generate_data.sh
@@ -7,23 +7,12 @@ for i in $(seq 1 $END); do
   echo "FIFO,$i,$HITRATE"
 done
 
-END="500"
-
 for i in $(seq 1 $END); do
   HITRATE=$(./paging-policy.py -f mem.txt -p LRU -C $i -c -N | grep FINALSTATS | awk '{print $7}')
   echo "LRU,$i,$HITRATE"
 done
 
-END="500"
-
 for i in $(seq 1 $END); do
   HITRATE=$(./paging-policy.py -f mem.txt -p MRU -C $i -c -N | grep FINALSTATS | awk '{print $7}')
   echo "MRU,$i,$HITRATE"
-done
-
-END="500"
-
-for i in $(seq 1 $END); do
-  HITRATE=$(./paging-policy.py -f mem.txt -p OPT -C $i -c -N | grep FINALSTATS | awk '{print $7}')
-  echo "OPT,$i,$HITRATE"
 done
