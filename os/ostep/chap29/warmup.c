6cd2b94ad2dc0a9ac8d796007589ee13d91b82a8
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-05-26 06:57:54 -0400

    finish exercise one

--- os/ostep/chap29/warmup.c
+++ os/ostep/chap29/warmup.c
@@ -1,5 +1,9 @@
-#include <sys/time.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 typedef struct __counter_t {
     int value;
@@ -21,6 +25,11 @@ int get(counter_t *c) {
     return c->value;
 }
 
+void count(counter_t *c, int limit) {
+    if (get(c) >= limit) return;
+    increment(c);
+}
+
 int main(int argc, char *argv[]) {
     counter_t counter;
     struct timeval tpBefore;
@@ -35,13 +44,16 @@ int main(int argc, char *argv[]) {
             exit(1);
             break;
         case 0: // Child
-            return count(counter, 100);
+            count(&counter, 100);
+            return 0;
         default: // Parent
+            ;
         }
 
     }
     while(wait(NULL)>0) ;
     gettimeofday(&tpAfter, NULL);
     long int total = tpAfter.tv_usec - tpBefore.tv_usec;
-    printf("Time taken: %d\n", total);
+    printf("Count: %d\n", get(&counter));
+    printf("Time taken: %d\n microseconds", total);
 }
