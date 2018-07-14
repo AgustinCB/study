c46dfc63cf86fcef60a2e5528111b389eb737420
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-07-14 07:10:23 -0400

    hlox is comming to live

--- compilers/craftinginterpreters/hlox/app/Hlox.hs
+++ compilers/craftinginterpreters/hlox/app/Hlox.hs
@@ -1,2 +1,19 @@
+import System.Environment
+
+usage :: String
+usage = "usage: hlox [script]"
+
+parse :: [String] -> IO()
+parse []      = runRepl
+parse ["-h"]  = print usage
+parse [file]  = runFile
+parse _       = print usage
+
+runRepl :: IO()
+runRepl = print "THIS IS A REPL"
+
+runFile :: IO()
+runFile = print "THIS IS A FILE"
+
 main :: IO ()
-main = print "HELLO"
+main = getArgs >>= parse
