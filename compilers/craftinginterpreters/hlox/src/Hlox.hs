68695fd244679207ae91c2ffe6b837aa912ad267
Agustin Chiappe Berrini <jnieve@gmail.com>
2018-07-26 18:58:14 -0400

    more work on scanner

--- compilers/craftinginterpreters/hlox/src/Hlox.hs
+++ compilers/craftinginterpreters/hlox/src/Hlox.hs
@@ -41,50 +41,52 @@ data TokenType a =
   While |
   Identifier String |
   StringLiteral String |
-  Number a
-data Token a = Token { tokenType :: TokenType a, lexeme :: String, tokenLocation :: SourceCodeLocation } deriving Show
+  Number a deriving Show
+data Token a = Token {
+    tokenType :: TokenType a
+  , lexeme :: String
+  , tokenLocation :: SourceCodeLocation } deriving Show
 
 type TokenResult a = (Token a, String, Int)
 
 -- Program results
 
-data SourceCodeLocation = SourceCodeLocation { file :: Maybe String, line :: Int }
-data ParseOutcome = Error { location :: SourceCodeLocation, msg :: String } | Success [Token]
+data SourceCodeLocation = SourceCodeLocation { file :: Maybe String, line :: Int } deriving Show
+data ParseOutcome a = Error { location :: SourceCodeLocation, msg :: String } | Success [Token a]
 
-instance Show SourceCodeLocation where
-  show (SourceCodeLocation (Just file) line) = "[line " ++ (show line) ++ "] Error in " ++ file ++ ": "
-  show (SourceCodeLocation Nothing line) = "[line " ++ (show line) ++ "] Error: "
 
-instance Show ParseOutcome where
-  show (Error location msg) = (show location) ++ msg
+instance Show a => Show (ParseOutcome a) where
+  show (Error (SourceCodeLocation (Just file) line)) = "[line " ++ (show line) ++ "] Error in " ++ file ++ ": "
+  show (Error (SourceCodeLocation Nothing line)) = "[line " ++ (show line) ++ "] Error: "
   show (Success []) = ""
   show (Success t:rest) = (show Token) ++ " " ++ (show (Success rest))
 
-createToken :: (Num a) => Char -> String -> Int -> TokenResult a
+createToken :: (Num a) => Char -> String -> Int -> Either SourceCodeLocation (TokenResult a)
 createToken nextChar rest line
-  | nextChar == '('   = oneCharToken LeftParen
-  | nextChar == ')'   = oneCharToken RightParen
-  | nextChar == '{'   = oneCharToken LeftBrace
-  | nextChar == '}'   = oneCharToken RightBrace
-  | nextChar == ','   = oneCharToken Comma
-  | nextChar == '.'   = oneCharToken Dot
-  | nextChar == '-'   = oneCharToken Minus
-  | nextChar == '+'   = oneCharToken Plus
-  | nextChar == ';'   = oneCharToken Semicolon
-  | nextChar == '*'   = oneCharToken Star
+  | nextChar == '('   = oneCharToken $ Right LeftParen
+  | nextChar == ')'   = oneCharToken $ Right RightParen
+  | nextChar == '{'   = oneCharToken $ Right LeftBrace
+  | nextChar == '}'   = oneCharToken $ Right RightBrace
+  | nextChar == ','   = oneCharToken $ Right Comma
+  | nextChar == '.'   = oneCharToken $ Right Dot
+  | nextChar == '-'   = oneCharToken $ Right Minus
+  | nextChar == '+'   = oneCharToken $ Right Plus
+  | nextChar == ';'   = oneCharToken $ Right Semicolon
+  | nextChar == '*'   = oneCharToken $ Right Star
   | nextChar == '\n'  = createToken (head rest) (tail rest) (line + 1)
+  | otherwise         = Left $ SourceCodeLocation Nothing line
   where
     oneCharToken :: TokenType a -> TokenResult a
     oneCharToken t = (Token t (nextChar : []) (SourceCodeLocation Nothing line), rest, line)
 
-scanToken :: (Num a) => String -> Int -> TokenResult a
+scanToken :: (Num a) => String -> Int -> Either SourceCodeLocation (TokenResult a)
 scanToken s l = createToken (head s) (tail s) l
 
-scanTokens :: (Num a) => String -> [Token a]
+scanTokens :: (Num a) => String -> ParseOutcome a
 scanTokens s = scanTokens' s 1
   where
-    scanTokens' :: (Num a) => String -> Int -> [Token a]
-    scanTokens' "" _ = []
-    scanTokens' s l =
-      let (token, rest, line) = scanToken s l
-      in token : scanTokens' rest line
+    scanTokens' :: (Num a) => String -> Int -> ParseOutcome a
+    scanTokens' "" _ = Success []
+    scanTokens' s l = case r of Left s@(SourceCodeLocation _ _) -> Error s "Unexpected character"
+                                Right (token, rest, line) -> token : scanTokens' rest line
+      where r = scanToken s l
