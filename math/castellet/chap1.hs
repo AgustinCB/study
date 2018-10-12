e6904806911f5713b58e89f4cbb455c7cb013ffe
Agustin Chiappe Berrini <agustin@auvik.com>
2018-10-12 15:11:31 -0400

    half way through exercise 17

--- math/castellet/chap1.hs
+++ math/castellet/chap1.hs
@@ -10,3 +10,17 @@ mcd a b
 
 mcm :: Integer -> Integer -> Integer
 mcm a b = (a * b) `div` (mcd a b)
+
+-- Exercise 17
+
+diofantic :: Integer -> Integer -> Integer -> Either String (Integer, Integer)
+diofantic a b
+  | (mod d c) != 0  = Left "This equation doesn't have solution"
+  | c != d          = do
+                        (r, s) <- diofantic a b d
+                        Right (r * c', s * c')
+  | otherwise       = 
+  where d = mcd a b
+        a' = a `div` d
+        b' = b `div` d
+        c'  = c `div` d
