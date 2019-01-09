6eb76a14432cd80b3ec8accf407db730afb8c611
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-01-09 06:40:50 -0500

    More cases to partition

--- compilers/craftinginterpreters/hlox/src/Interpreter.hs
+++ compilers/craftinginterpreters/hlox/src/Interpreter.hs
@@ -47,7 +47,7 @@ partitionScope :: LoxState -> (Maybe LoxState, Maybe LoxState)
 partitionScope = partitionScope' Nothing
   where partitionScope' :: Maybe LoxState -> LoxState -> (Maybe LoxState, Maybe LoxState)
         partitionScope' Nothing base@(LoxState _ _ Nothing False _) = (Just base, Nothing)
-        partitionScope' Nothing base@(LoxState _ _ Nothing True _) = (Nothing, Just base)
+        partitionScope' Nothing base@(LoxState _ _ parent True _) = (parent, Just base)
         partitionScope' Nothing base@(LoxState _ _ (Just parent) False _) = partitionScope' (Just base) parent
 
 isTruthy :: LoxValue -> LoxValue
